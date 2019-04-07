cedb6a0d8a5fbf3ce877a5054a715e2602e86abd
lujian <lujian282012@163.com>
2019-04-07 18:22:49 +0800

    update parameter

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import pickle
 import keras
 import bilsm_crf_model
 
-EPOCHS = 10
+EPOCHS = 100
 model, (train_x, train_y), (test_x, test_y) = bilsm_crf_model.create_model()
 # train model
 history = model.fit(train_x, train_y, batch_size=16, epochs=EPOCHS, validation_data=[test_x, test_y],
