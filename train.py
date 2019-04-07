8ac6813d31ad920faae3af9ab91b042435c8644f
lujian <lujian282012@163.com>
2019-04-07 17:10:49 +0800

    update parameter

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 import bilsm_crf_model
 import pickle
 
-EPOCHS = 5
+EPOCHS = 10
 model, (train_x, train_y), (test_x, test_y) = bilsm_crf_model.create_model()
 # train model
-history = model.fit(train_x[:1000], train_y[:1000], batch_size=16, epochs=EPOCHS, validation_data=[test_x, test_y])
+history = model.fit(train_x, train_y, batch_size=16, epochs=EPOCHS, validation_data=[test_x, test_y])
 model.save('model/crf.h5')
 
 with open('model/history.pkl', 'wb') as wd:
