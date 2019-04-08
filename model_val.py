f67ad5171c4862c78ee0618647cb148084c1435c
lujian <lujian282012@163.com>
2019-04-08 09:34:40 +0800

    update parameter

--- model_val.py
+++ model_val.py
@@ -5,6 +5,7 @@ with open('model/history.pkl', 'rb') as wd:
     history = pickle.load(wd)
     wd.close()
     hist = pd.DataFrame(history)
-    hist.loc[:, ['crf_viterbi_accuracy', 'val_crf_viterbi_accuracy']].plot(kind='line')
+    hist.loc[:, ['crf_viterbi_accuracy']].plot(kind='line')
+    hist.loc[:, ['val_crf_viterbi_accuracy']].plot(kind='line')
     hist.loc[:, ['loss']].plot(kind='line')
     hist.loc[:, ['val_loss']].plot(kind='line')
--- train.py
+++ train.py
@@ -2,10 +2,12 @@ import pickle
 import keras
 import bilsm_crf_model
 
-EPOCHS = 100
+EPOCHS = 10
 model, (train_x, train_y), (test_x, test_y) = bilsm_crf_model.create_model()
 # train model
-history = model.fit(train_x, train_y, batch_size=16, epochs=EPOCHS, validation_data=[test_x, test_y],
+split = 6000
+history = model.fit(train_x[:split], train_y[:split], batch_size=16, epochs=EPOCHS,
+                    validation_data=[train_x[split:], train_y[split:]],
                     callbacks=[
                         # keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='auto'),
                         keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=1, batch_size=128)
